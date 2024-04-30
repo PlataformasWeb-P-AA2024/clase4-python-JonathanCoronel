93c3ed710f34736ca593e53766749cd382344ba5
Jonathan Coronel <jhonath.co@gmail.com>
2024-04-29 23:43:50 -0500

    tarea

--- ejemplo10.py
+++ ejemplo10.py
@@ -4,9 +4,11 @@ archivo = open("data/atp_tennis.csv","r")
 lineas = archivo.readlines()
 lineas = [l.split("|") for l in lineas]
 
-for x in lineas:
+num = 0 
+for x in lineas[1:]:
     cadena = """<b>Torneo:</b> %s <br> <b>Ganador:</b> %s""" % (x[0],x[9])
+    num = num+1
     print(cadena)
-    archivo_generado = open("data/%s.html" % (x[9]),"w")
+    archivo_generado = open("data/Ganador-%d-%s.html" % (num,x[9]),"w")
     archivo_generado.writelines("%s\n" % (cadena))
     archivo_generado.close
